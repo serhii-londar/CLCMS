4808ad3fa6d244eb5a328a1b3fe0439f5954a831
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-09 02:42:07 -0500

    Updated for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,7 +1,10 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "CLCMS",
     pkgConfig: "lcms2",
-    providers: [.Brew("lcms2"), .Apt("lcms2")]
+    providers: [.brew(["lcms2"]), .apt(["lcms2"])]
 )
