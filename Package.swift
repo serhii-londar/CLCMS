c228e6dee8661109f0b72aa20cf2799ea5962400
Serhii Londar <serhii.londar@gmail.com>
2018-10-19 01:31:53 +0300

    Swift 4.2 Support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CLCMS",
-    pkgConfig: "lcms2",
-    providers: [.brew(["lcms2"]), .apt(["lcms2"])]
+    products: [ .library(name: "CLCMS", targets: ["CLCMS"]) ],
+    targets: [ .systemLibrary(name: "CLCMS", pkgConfig: "lcms2", providers: [ .brew(["lcms2"]), .apt(["lcms2"])]) ]
 )
